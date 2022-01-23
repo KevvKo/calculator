017439b8288d770439c6d295ac9804e4dcb6b8c9
KevvKo <kklein915@gmail.com>
2022-01-23 21:07:12 +0100

    first simple calculator

--- main.cpp
+++ main.cpp
@@ -3,6 +3,35 @@ using namespace::std;
 
 int main(){
 
-    cout << "hello";
+    int num1, num2;
+    char operand;
+
+    cout << "Enter a number:";
+    cin >> num1;
+    
+    cout << "Enter an operator:" << std::endl;
+    cin >> operand;
+
+    cout << "Enter next operand:" << std::endl;
+    cin >> num2;
+
+    switch(operand){
+        case '+':
+            cout << "Result: " << num1 + num2;
+            break;
+        
+        case '-':
+            cout << "Result: " << num1 - num2;
+            break;
+        
+        case '*':
+            cout << "Result: " << num1 * num2;
+            break;
+        
+        case '/':
+            cout << "Result: " << num1 / num2;
+            break;
+    }
+    
     return 0;   
 }
